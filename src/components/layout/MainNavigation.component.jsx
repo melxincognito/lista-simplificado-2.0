3c0261136d2955ac995c782f63b3a0105baaaff2
Mel Incognito <melxincognito@gmail.com>
2021-11-21 23:28:42 -0700

    initial commit. rendering Navigation bar

---
+++ src/components/layout/MainNavigation.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Listas Simplificado</div>
+      
+        <ul>
+          <li>Todos mis tareas</li>
+          <li>Tareas Importantes</li>
+          <li>Añadir Tarea</li>
+        </ul>
+      
+    </header>
+  );
+}
+
+export default MainNavigation;
+
