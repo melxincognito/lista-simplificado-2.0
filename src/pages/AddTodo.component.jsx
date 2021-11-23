94eaa37d599ca193eabe797f5b7a25d41f3330e5
Mel Incognito <melxincognito@gmail.com>
2021-11-22 23:36:28 -0700

    Hooked up to Firebase as backend server, form data sending correctly to server

--- src/pages/AddTodo.component.jsx
+++ src/pages/AddTodo.component.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import NewTodoForm from "../components/form/NewTodoForm.component";
+import { useNavigate } from 'react-router-dom';
+
 
 function AddTodosPage() {
+  const navigate = useNavigate();
+
+  function addTodoHandler(todoData) {
+    fetch(
+      "https://lista-simplificado-2-default-rtdb.firebaseio.com/todosMisTareas.json",
+      {
+        method: "POST",
+        body: JSON.stringify(todoData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate('/')
+    });
+  }
+
   return (
     <div>
-      <h1> Agregar Tarea </h1>
-      <NewTodoForm/>
+      <h1>Añadir Tarea</h1>
+      <div>
+           <NewTodoForm onAddTodo={addTodoHandler}/>
+      </div>
+
+   
     </div>
   );
 }
